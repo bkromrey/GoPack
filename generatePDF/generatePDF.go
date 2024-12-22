4ef28a071e13f7bd85700487fec2fa84e0a90e60
bkromrey <146262682+bkromrey@users.noreply.github.com>
2024-12-22 11:52:30 -0600

    implemented PDF export

--- generatePDF/generatePDF.go
+++ generatePDF/generatePDF.go
@@ -6,6 +6,7 @@ import (
 	"github.com/johnfercher/maroto/v2/pkg/components/row"
 	"github.com/johnfercher/maroto/v2/pkg/config"
 	"github.com/johnfercher/maroto/v2/pkg/consts/align"
+	"github.com/johnfercher/maroto/v2/pkg/consts/fontstyle"
 	"github.com/johnfercher/maroto/v2/pkg/core"
 	"github.com/johnfercher/maroto/v2/pkg/props"
 	"log"
@@ -32,14 +33,43 @@ func getMaroto(list fileHandling.PackingList) core.Maroto {
 		WithRightMargin(10).
 		Build()
 
-	mrt := maroto.New(pdfConfig)
+	pdf := maroto.New(pdfConfig)
 
-	err := mrt.RegisterHeader(getPageHeader(list))
-	if err != nil {
-		log.Fatal(err)
-	}
+	pdf.AddRow(7,
+		col.New(3),
+		col.New(6).Add(
+			text.New(list.ListName, props.Text{
+				Size:  14,
+				Align: align.Center,
+				Style: fontstyle.Bold,
+			}),
+		),
+		col.New(3))
 
-	return mrt
+	pdf.AddRow(6,
+		col.New(3),
+		col.New(6).Add(
+			text.New(list.Destination, props.Text{
+				Size:  12,
+				Align: align.Center,
+			}),
+		),
+		col.New(3))
+
+	pdf.AddRow(4,
+		col.New(3),
+		col.New(6).Add(
+			text.New(list.DepartDate+" to "+list.ReturnDate, props.Text{
+				Size:  10,
+				Align: align.Center,
+			}),
+		))
+
+	// include packing list contents
+	pdf.AddRow(5)
+	pdf.AddRows(getItemList(list.Contents)...)
+
+	return pdf
 }
 
 // GeneratePDF is a function used to parse a PackingList object into a PDF file
@@ -71,20 +101,49 @@ func GeneratePDF(list fileHandling.PackingList) (string, string) {
 	return saveLocation, fmt.Sprintf("%v successfully exported.", list.ListName)
 }
 
-// PDF CONTENTS -------------------------------------------------------------------------------------------------------
-func getPageHeader(list fileHandling.PackingList) core.Row {
-	return row.New(20).Add(
-		col.New(3),
-		col.New(6).Add(
-			text.New(list.ListName, props.Text{
-				Size:  8,
-				Align: align.Center,
-			}),
-			text.New(list.DepartDate+" - "+list.ReturnDate, props.Text{
-				Size:  5,
-				Align: align.Center,
-			}),
+//// PDF CONTENTS -------------------------------------------------------------------------------------------------------
+
+func getItemList(contents []fileHandling.ListItem) []core.Row {
+	rows := []core.Row{
+
+		// setup header columns for packing list
+		row.New(5).Add(
+			col.New(2),
+			//text.NewCol(1),
+			text.NewCol(4, "Item", props.Text{Style: fontstyle.Bold}),
+			text.NewCol(3, "Category", props.Text{Style: fontstyle.Bold}),
+			text.NewCol(3, "Packed Location", props.Text{Style: fontstyle.Bold}),
+			//col.New(1),
 		),
-		col.New(3),
-	)
+		row.New(3),
+	}
+
+	// iterate over items in the list and generate a row for each
+	var itemRows []core.Row
+
+	for _, currentItem := range contents {
+
+		var status string
+
+		if currentItem.Packed {
+			status = "[x]"
+		} else {
+			status = "[  ]"
+		}
+
+		currentRow := row.New(5).Add(
+			col.New(1),
+			text.NewCol(1, status),
+			text.NewCol(4, currentItem.ItemName),
+			text.NewCol(3, currentItem.ItemCategory),
+			text.NewCol(3, currentItem.ItemLocation),
+			//col.New(1),
+		)
+
+		itemRows = append(itemRows, currentRow)
+	}
+
+	// combine header & content rows
+	rows = append(rows, itemRows...)
+	return rows
 }
